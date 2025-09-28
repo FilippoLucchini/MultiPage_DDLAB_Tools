319c5e01c971c715c4cdee1bb71432d83f6fc84e
FilippoLucchini <111346438+FilippoLucchini@users.noreply.github.com>
2025-09-28 23:56:27 +0200

    Update Reagents_Database.py

--- pages/Reagents_Database.py
+++ pages/Reagents_Database.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import os
 
+# Set app layout on wide
+st.set_page_config(layout="wide")
+
 st.title("⚗️ Reagents Database")
 
 file_path = "Reagents_Database.xlsx"
