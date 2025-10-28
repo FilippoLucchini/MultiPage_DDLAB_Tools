22a2cdeeb47999f2ea99efbc509ae3381645411b
FilippoLucchini <111346438+FilippoLucchini@users.noreply.github.com>
2025-10-28 20:22:03 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -85,7 +85,7 @@ with db_col1:
 
 with db_col2:
     if st.button("More to Come", use_container_width=True):
-        st.switch_page("pages/07_Calculators.py")
+        st.switch_page("pages")
     st.markdown('<div class="card"><h3>Future features to come</h3><p>Lets see what the future brings us!.</p></div>', unsafe_allow_html=True)
 
 with db_col3:
@@ -111,7 +111,19 @@ with tools_col2:
         st.switch_page("pages/05_MasterMix Calculator.py")
     st.markdown('<div class="card"><h3>🥣 Master Mix</h3><p>Calculate reagents mixes.</p></div>', unsafe_allow_html=True)
 
+tools_col1, tools_col2 = st.columns(2) 
+
+with tools_col1:
+    if st.button("🧮 Calculators Tool", use_container_width=True):
+        st.switch_page("pages/07_Calculators.py")
+    st.markdown('<div class="card"><h3>🧮 Calculators Tool</h3><p>Dilution, Buffer and Molarity calculator.</p></div>', unsafe_allow_html=True)
+
+with tools_col2:
+    if st.button("Future Tool", use_container_width=True):
+        st.switch_page("pages")
+    st.markdown('<div class="card"><h3>Future Tool</h3><p>TBD</p></div>', unsafe_allow_html=True)
 st.markdown("---")  # horizontal divider
+
 
 
 
